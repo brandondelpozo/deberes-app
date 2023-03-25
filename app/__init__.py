e2206b356e7c4910ba6aeff00d189fc96276d477
brandondelpozo <branzzel@gmail.com>
2023-03-25 17:20:25 -0500

    add context

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,19 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import os
 
+# create the Flask app instance
 app = Flask(__name__)
-
-db = SQLAlchemy(app)
+# set the secret key for the app
+app.config['SECRET_KEY'] = os.urandom(32)
+# configure the database URI
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sqlite.db'
-SECRET_KEY = os.urandom(32)
-app.config['SECRET_KEY'] = 'SECRET_KEY'
+# initialize the database with the app
+db = SQLAlchemy(app)
+# import views and models
+from . import views, models
 
-
-from app import views
-from app import models
-
-db.init_app(app)
-db.create_all()
+# wrap the database initialization in an application context
+with app.app_context():
+    # create the database tables
+    db.create_all()
